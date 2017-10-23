f042a2065f3a5648e7aa661e755a3d22fd8dda91
latene <l.atene@gmail.com>
2017-10-23 18:15:00 +0200

    rimossi commenti con caratteri speciali nell'ObjectFactory.java

--- JedisCacheAWS.module/src/xmlManagment/ObjectFactory.java
+++ JedisCacheAWS.module/src/xmlManagment/ObjectFactory.java
@@ -1,11 +1,3 @@
-//
-// Questo file è stato generato dall'architettura JavaTM per XML Binding (JAXB) Reference Implementation, v2.2.8-b130911.1802 
-// Vedere <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Qualsiasi modifica a questo file andrà persa durante la ricompilazione dello schema di origine. 
-// Generato il: 2017.10.04 alle 01:01:02 PM CEST 
-//
-
-
 package xmlManagment;
 
 import javax.xml.bind.JAXBElement;
